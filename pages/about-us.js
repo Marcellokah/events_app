ef8b814f8d73af0d027d623b08de67c1c8ac9da1
Marcellokah <aalma47@gmail.com>
2022-12-19 13:25:07 +0100

    Basic pages

---
+++ pages/about-us.js
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Cillum amet fugiat eiusmod fugiat ullamco adipisicing dolore dolor occaecat. In dolor nulla elit aliquip mollit eu Lorem minim qui. Pariatur dolore pariatur excepteur consectetur officia ipsum minim nostrud minim et sunt. Incididunt irure laborum ut velit veniam minim deserunt laborum ullamco pariatur ex proident ullamco duis. Est magna nisi sunt enim culpa veniam proident id deserunt enim quis. Officia officia ipsum magna duis occaecat nulla anim consectetur elit ut veniam magna ullamco.</p>
+        </div>
+    );
+}
+
+export default Page;
---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <h1>Event page</h1>
+    );
+}
+
+export default Page;
